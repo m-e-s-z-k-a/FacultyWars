6767c7fe8301f806ebd6bf0899c38f450f8229a9
m-e-s-z-k-a <92521745+m-e-s-z-k-a@users.noreply.github.com>
2022-02-04 20:56:42 +0100

    init

---
+++ src/main/java/agh/ics/oop/Map.java
@@ -0,0 +1,15 @@
+package agh.ics.oop;
+
+public class Map
+{
+    public static int width;
+    public static int height;
+
+public Map(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+
+}
